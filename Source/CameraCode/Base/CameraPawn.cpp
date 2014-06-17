12f1207e3a1a425fe8dc2a65ea1f0c9dd233e7ae
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-06-17 08:20:51 +0200

    Base cmaera code to control "zoom" and pitch angle

--- Source/CameraCode/Base/CameraPawn.cpp
+++ Source/CameraCode/Base/CameraPawn.cpp
@@ -3,11 +3,47 @@
 #include "../CameraCode.h"
 #include "CameraPawn.h"
 
+#include <UnrealMath.h>
+
+#include <assert.h>
 
 ACameraPawn::ACameraPawn(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+    Sphere = PCIP.CreateDefaultSubobject<USphereComponent>(this, TEXT("Sphere"));
+    Sphere->InitSphereRadius(5.0f);
+    RootComponent = Sphere;
+    
+    CameraArm = PCIP.CreateDefaultSubobject<USpringArmComponent>(this, TEXT("SpringArm"));
+    CameraArm->AttachParent = RootComponent;
+
+    MainCamera = PCIP.CreateDefaultSubobject<UCameraComponent>(this, TEXT("Camera"));
+    MainCamera->AttachParent = CameraArm;
+
+    this->MaxDistance = 2000; // 20 meters
+    this->MinDistance = 100;  // 1 meters
+    this->MaxPitch = -66;
+    this->MinPitch = -45;
+
+    this->Yaw = 0;
+    this->Roll = 0;
+
+    this->SetCameraDistance(1.0);
+
 
 }
 
+void ACameraPawn::SetCameraDistance(float distance)
+{
+    assert(distance < 1.0 && distance >= 0);
+
+    // Gets distance and pitch angle from max/min and the normalized value
+    this->FocusDistance = FMath::Lerp(this->MinDistance, this->MaxDistance, distance);
+    this->Pitch = FMath::Lerp(this->MinPitch, this->MaxPitch, distance);
+
+    CameraArm->TargetArmLength = this->FocusDistance;
+
+    FRotator rot(this->Pitch, this->Yaw, this->Roll);
+    CameraArm->SetRelativeRotation(rot, true);
+}
 
--- Source/CameraCode/Base/CameraPawn.h
+++ Source/CameraCode/Base/CameraPawn.h
@@ -6,13 +6,66 @@
 #include "CameraPawn.generated.h"
 
 /**
- * 
+ * 3d Camera Pawn that spins around Z axis, moves along the XY plane and Z positions are fixed to "floors"
+ * It's centered on a focus point, that is were, in 3d world, will be centered the camera
  */
 UCLASS()
 class ACameraPawn : public APawn
 {
 	GENERATED_UCLASS_BODY()
 
-	
-	
+    /** point light component */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Components")
+    TSubobjectPtr<USphereComponent> Sphere;
+
+    /** Spring Arm were the camera is attached */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Components")
+    TSubobjectPtr<USpringArmComponent> CameraArm;
+
+    /** Camera */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+    TSubobjectPtr<UCameraComponent> MainCamera;
+
+
+    /** Camera Max distance from the focus point */
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Camera Position")
+    float MaxDistance;
+
+    /** Camera Min distance from the focus point */
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Camera Position")
+    float MinDistance;
+
+    /** Camera distance from the focus point */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera Position")
+    float FocusDistance;
+    
+    /** Max Pitch Angle at were the camera is located respect focus point when the camera is far*/
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Camera Position")
+    float MaxPitch;
+
+    /** Min Pitch Angle at were the camera is located respect focus point when the camera is near*/
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Camera Position")
+    float MinPitch;
+
+    /** Pitch Angle at were the camera is located respect focus point*/
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera Position")
+    float Pitch;
+
+    /** Yaw Angle at were the camera is located respect focus point*/
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Position")
+    float Yaw;
+
+    /** Roll Angle at were the camera is located respect focus point*/
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera Position")
+    float Roll;
+
+    /**
+     * Sets the camera distance between max and min distance
+     * @param distance Normalized distance, were 1.0 is the max distance and 0.0 is the minimal distance 
+     */
+    UFUNCTION(BlueprintCallable, Category = "Camera Position")
+    void SetCameraDistance(float distance);
+
+
+
 };
